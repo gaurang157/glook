09f8b59b67a481f1420172a3d57161f8917f129b
Gaurang Ingle <148379526+gaurang157@users.noreply.github.com>
2024-04-22 23:19:35 +0530

    Update 2_Univariate_Analysis.py

--- glook/pages/2_Univariate_Analysis.py
+++ glook/pages/2_Univariate_Analysis.py
@@ -1,162 +1,172 @@
 # page2.py
-import streamlit as st
-st.set_page_config(
-		page_title="ML-Automation", 
-		page_icon="🏗️", 
-		layout="wide", 
-		initial_sidebar_state = "expanded")
+try:
+	import streamlit as st
+	st.set_page_config(
+			page_title="ML-Automation", 
+			page_icon="🏗️", 
+			layout="wide", 
+			initial_sidebar_state = "expanded")
 
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import plotly.express as px
-import plotly.graph_objects as go
-import numpy as np
-import statsmodels.api as sm
-st.set_option('deprecation.showPyplotGlobalUse', False)
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
+	import pandas as pd
+	import seaborn as sns
+	import matplotlib.pyplot as plt
+	import plotly.express as px
+	import plotly.graph_objects as go
+	import numpy as np
+	import statsmodels.api as sm
+	st.set_option('deprecation.showPyplotGlobalUse', False)
+	import matplotlib
+	matplotlib.use('Agg')
+except Exception as e:
+	print("z", e)
 
+# from wordcloud import WordCloud
+# import matplotlib.pyplot as plt
+# import matplotlib
+# matplotlib.use('TkAgg')
 # st.title("1️⃣Univariate Analysis")
 
+
+st.write("Session State:->", st.session_state["shared"])
+# st.session_state["shared"]
+# Function to limit the number of unique values for a categorical column
+def limit_unique_values(series, limit=20):
+	if series.nunique() > limit:
+		top_categories = series.value_counts().index[:limit]
+		return series.apply(lambda x: x if x in top_categories else 'Other')
+	else:
+		return series
+
+def limit_unique_values_cloud(series, limit=200):
+	if series.nunique() > limit:
+		top_categories = series.value_counts().index[:limit]
+		return top_categories
+	else:
+		return series.unique()
+
+# def limit_unique_values_cloud(series, limit=200):
+# 	if series.nunique() > limit:
+# 		top_categories = series.value_counts().index[:limit]
+# 		return series.apply(lambda x: x if x in top_categories else 'Other')
+# 	else:
+# 		return series
+
+
+
+def calculate_insights(column_data):
+	insights = {}
+
+	# Count of distinct values
+	insights['Distinct'] = len(column_data.dropna().unique())
+
+	# Percentage of distinct values
+	insights['Distinct (%)'] = len(column_data.dropna().unique()) / len(column_data.dropna()) * 100
+
+	# Count of missing values
+	insights['Missing'] = column_data.isnull().sum()
+
+	# Percentage of missing values
+	insights['Missing (%)'] = column_data.isnull().sum() / len(column_data) * 100
+
+	# Count of infinite values
+	insights['Infinite'] = np.isinf(column_data).sum()
+
+	# Percentage of infinite values
+	insights['Infinite (%)'] = np.isinf(column_data).sum() / len(column_data) * 100
+
+	# Mean
+	insights['Mean'] = column_data.mean()
+
+	# Median
+	insights['Median'] = column_data.median()
+
+	# Mode
+	insights['Mode'] = column_data.mode().iloc[0]
+
+	# Minimum
+	insights['Minimum'] = column_data.min()
+
+	# Maximum
+	insights['Maximum'] = column_data.max()
+
+	# Zeros count
+	insights['Zeros'] = (column_data == 0).sum()
+
+	# Percentage of zeros
+	insights['Zeros (%)'] = (column_data == 0).sum() / len(column_data) * 100
+
+	# Negative values count
+	insights['Negative'] = (column_data < 0).sum()
+
+	# Percentage of negative values
+	insights['Negative (%)'] = (column_data < 0).sum() / len(column_data) * 100
+
+	# Memory size
+	insights['Memory size'] = column_data.memory_usage(deep=True)
+
+	# 5th percentile
+	insights['5-th percentile'] = np.percentile(column_data.dropna(), 5)
+
+	# Q1 (25th percentile)
+	insights['Q1'] = np.percentile(column_data.dropna(), 25)
+
+	# Median
+	insights['Median'] = np.median(column_data.dropna())
+
+	# Q3 (75th percentile)
+	insights['Q3'] = np.percentile(column_data.dropna(), 75)
+
+	# 95th percentile
+	insights['95-th percentile'] = np.percentile(column_data.dropna(), 95)
+
+	# Range
+	insights['Range'] = insights['Maximum'] - insights['Minimum']
+
+	# Interquartile range (IQR)
+	insights['Interquartile range'] = insights['Q3'] - insights['Q1']
+
+	# Descriptive statistics
+	insights['Descriptive statistics'] = column_data.describe()
+
+	# Standard deviation
+	insights['Standard deviation'] = column_data.std()
+
+	# Coefficient of variation (CV)
+	insights['Coefficient of variation (CV)'] = insights['Standard deviation'] / insights['Mean']
+
+	# Kurtosis
+	insights['Kurtosis'] = column_data.kurtosis()
+
+	# Median Absolute Deviation (MAD)
+	mad = np.median(np.abs(column_data.dropna() - np.median(column_data.dropna())))
+	insights['Median Absolute Deviation (MAD)'] = mad
+
+	# Skewness
+	insights['Skewness'] = column_data.skew()
+
+	# Sum
+	insights['Sum'] = column_data.sum()
+
+	# Variance
+	insights['Variance'] = column_data.var()
+
+	# Monotonicity (not calculated)
+
+	return insights
+
+
+# if "df" in st.session_state:
+# 	df = st.session_state.df
+
+
+
 try:
-	st.write("Session State:->", st.session_state["shared"])
-	# st.session_state["shared"]
-	# Function to limit the number of unique values for a categorical column
-	def limit_unique_values(series, limit=20):
-		if series.nunique() > limit:
-			top_categories = series.value_counts().index[:limit]
-			return series.apply(lambda x: x if x in top_categories else 'Other')
-		else:
-			return series
-
-	def limit_unique_values_cloud(series, limit=200):
-		if series.nunique() > limit:
-			top_categories = series.value_counts().index[:limit]
-			return top_categories
-		else:
-			return series.unique()
-
-	# def limit_unique_values_cloud(series, limit=200):
-	# 	if series.nunique() > limit:
-	# 		top_categories = series.value_counts().index[:limit]
-	# 		return series.apply(lambda x: x if x in top_categories else 'Other')
-	# 	else:
-	# 		return series
-
-
-
-	def calculate_insights(column_data):
-	    insights = {}
-
-	    # Count of distinct values
-	    insights['Distinct'] = len(column_data.dropna().unique())
-
-	    # Percentage of distinct values
-	    insights['Distinct (%)'] = len(column_data.dropna().unique()) / len(column_data.dropna()) * 100
-
-	    # Count of missing values
-	    insights['Missing'] = column_data.isnull().sum()
-
-	    # Percentage of missing values
-	    insights['Missing (%)'] = column_data.isnull().sum() / len(column_data) * 100
-
-	    # Count of infinite values
-	    insights['Infinite'] = np.isinf(column_data).sum()
-
-	    # Percentage of infinite values
-	    insights['Infinite (%)'] = np.isinf(column_data).sum() / len(column_data) * 100
-
-	    # Mean
-	    insights['Mean'] = column_data.mean()
-
-	    # Median
-	    insights['Median'] = column_data.median()
-
-	    # Mode
-	    insights['Mode'] = column_data.mode().iloc[0]
-
-	    # Minimum
-	    insights['Minimum'] = column_data.min()
-
-	    # Maximum
-	    insights['Maximum'] = column_data.max()
-
-	    # Zeros count
-	    insights['Zeros'] = (column_data == 0).sum()
-
-	    # Percentage of zeros
-	    insights['Zeros (%)'] = (column_data == 0).sum() / len(column_data) * 100
-
-	    # Negative values count
-	    insights['Negative'] = (column_data < 0).sum()
-
-	    # Percentage of negative values
-	    insights['Negative (%)'] = (column_data < 0).sum() / len(column_data) * 100
-
-	    # Memory size
-	    insights['Memory size'] = column_data.memory_usage(deep=True)
-
-	    # 5th percentile
-	    insights['5-th percentile'] = np.percentile(column_data.dropna(), 5)
-
-	    # Q1 (25th percentile)
-	    insights['Q1'] = np.percentile(column_data.dropna(), 25)
-
-	    # Median
-	    insights['Median'] = np.median(column_data.dropna())
-
-	    # Q3 (75th percentile)
-	    insights['Q3'] = np.percentile(column_data.dropna(), 75)
-
-	    # 95th percentile
-	    insights['95-th percentile'] = np.percentile(column_data.dropna(), 95)
-
-	    # Range
-	    insights['Range'] = insights['Maximum'] - insights['Minimum']
-
-	    # Interquartile range (IQR)
-	    insights['Interquartile range'] = insights['Q3'] - insights['Q1']
-
-	    # Descriptive statistics
-	    insights['Descriptive statistics'] = column_data.describe()
-
-	    # Standard deviation
-	    insights['Standard deviation'] = column_data.std()
-
-	    # Coefficient of variation (CV)
-	    insights['Coefficient of variation (CV)'] = insights['Standard deviation'] / insights['Mean']
-
-	    # Kurtosis
-	    insights['Kurtosis'] = column_data.kurtosis()
-
-	    # Median Absolute Deviation (MAD)
-	    mad = np.median(np.abs(column_data.dropna() - np.median(column_data.dropna())))
-	    insights['Median Absolute Deviation (MAD)'] = mad
-
-	    # Skewness
-	    insights['Skewness'] = column_data.skew()
-
-	    # Sum
-	    insights['Sum'] = column_data.sum()
-
-	    # Variance
-	    insights['Variance'] = column_data.var()
-
-	    # Monotonicity (not calculated)
-
-	    return insights
-
-
-
-
-
-
-
 	# Assuming df is already defined and stored in session_state
-	if "df" in st.session_state:
+	
+	if "df" in st.session_state and st.session_state.df is not None:
 		df = st.session_state.df
-		
+		# import time
+		# s = time.time()
 		# # Get number of rows
 		# num_rows = df.shape[0]
 		
@@ -189,8 +199,31 @@ try:
 		# Display insights for each column
 		st.title("1️⃣ Univariate Analysis")
 		# st.header(":green[Column Insights:]⤵️")
+		# html_content = ""
 		for idx, column in enumerate(df.columns):
-			st.subheader(f":green[**Column {idx + 1}:** {column}]")
+			# st.subheader(f':green[**Column {idx + 1}:** {column}]')
+			# subh = f""":green[Column {idx + 1}: {column}]"""
+			# st.subheader(subh)
+			# subh = f"**Column {idx + 1}:** `{column}`"
+			# st.markdown(subh, unsafe_allow_html=True)
+			# subh = "**Column " + str(idx + 1) + ":** `" + column + "`"
+			# st.markdown(subh, unsafe_allow_html=True)
+			html_content = f"<div class='column-header'>Column {idx + 1}: <code>{column}</code></div>"
+
+			css_style = """
+			<style>
+			.column-header {
+				margin-bottom: 10px;
+				font-weight: bold;
+				font-size: 26px;
+				color: green; /* Change color as needed */
+			}
+			</style>
+			"""
+
+			# Embed HTML and CSS code into Streamlit
+			st.markdown(css_style, unsafe_allow_html=True)
+			st.markdown(html_content, unsafe_allow_html=True)
 			if df[column].dtype == 'object':
 				col1, col2 = st.columns(2)
 				with col1:
@@ -203,7 +236,7 @@ try:
 						st.write(f"  - Top 20 Unique Values:")
 						st.write(f":green[{', '.join(map(str, df[column].value_counts().head(20).index))}]")
 				with col2:
-					""" distinct plot """
+					# """ distinct plot """
 					plt.figure(figsize=(10, 6))  # Adjust figure size
 					try:
 						sns.countplot(x=limit_unique_values(df[column]), data=df, color="green")
@@ -212,9 +245,22 @@ try:
 					plt.xticks(rotation=45)  # Rotate x-axis labels
 					st.pyplot()
 					plt.close()
+	 				# Assuming df is your DataFrame and column is the specific column you want to plot
+					# def distinct_plot(df, column):
+					# 	try:
+					# 		fig = px.histogram(df, x=limit_unique_values(df[column]), color_discrete_sequence=["green"])
+					# 	except:
+					# 		fig = px.histogram(df, x=df[column], color_discrete_sequence=["green"])
+						
+					# 	fig.update_layout(xaxis_tickangle=-45)  # Rotate x-axis labels
+					# 	st.plotly_chart(fig, use_container_width=True)
+
+					# # Call the function with your DataFrame and column
+					# distinct_plot(df, column)
 				with st.expander("More Info"):
 					# st.write("More Info ⬇")
-					tab1, tab2, tab3 = st.tabs(["Insights", "Donut chart", "WordCloud"])
+					# tab1, tab2, tab3 = st.tabs(["Insights", "Donut chart", "WordCloud"])
+					tab1, tab2 = st.tabs(["Insights", "Donut chart"])
 					with tab1:
 						col7, col8, col9 = st.columns(3)
 						with col7:
@@ -259,56 +305,86 @@ try:
 						fig.update_traces(textposition='inside', textinfo='percent+label')
 						fig.update_layout(legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1))
 						st.plotly_chart(fig)
-
+						print("j")
 						# st.header("An owl")
 						# st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 
-					with tab3:
-						# Concatenate all values of the column into a single string
-						cleanstrng = ' '.join(df[column].dropna())
+					# with tab3:
+					# 	# Concatenate all values of the column into a single string
+					# 	cleanstrng = ' '.join(df[column].dropna())
 
-						# Check if there are words to plot
-						if cleanstrng:
-							# Generate word cloud
-							word_freq = pd.Series(cleanstrng.split()).value_counts()
+					# 	# Check if there are words to plot
+					# 	if cleanstrng:
+					# 		# Generate word cloud
+					# 		word_freq = pd.Series(cleanstrng.split()).value_counts()
 							
-							# Select top 200 words if available, otherwise take the exact number of words
-							if len(word_freq) >= 50:
-								top_words = word_freq.head(50)
-							else:
-								top_words = word_freq
+					# 		# Select top 200 words if available, otherwise take the exact number of words
+					# 		if len(word_freq) >= 50:
+					# 			top_words = word_freq.head(50)
+					# 		else:
+					# 			top_words = word_freq
 
-							words_dict = top_words.to_dict()
+					# 		words_dict = top_words.to_dict()
 							
-							try:
-								# Generate word cloud from frequencies
-								wordcloud_ip = WordCloud(background_color='white', width=2800, height=2400).generate_from_frequencies(words_dict)
+					# 		try:
+					# 			# Generate word cloud from frequencies
+					# 			wordcloud_ip = WordCloud(background_color='white', width=2800, height=2400).generate_from_frequencies(words_dict)
 
-								# Display word cloud
-								plt.figure(figsize=(10, 7))
-								plt.imshow(wordcloud_ip, interpolation='bilinear')
-								plt.title(f'Word Cloud for {column}')
-								plt.axis('off')
-								plt.show()
-								st.pyplot(plt, use_container_width=True)
-							except Exception as e:
-								st.write(f"Error generating word cloud: {e}")
-						else:
-							st.write(f"No words to plot for column '{column}'")
+					# 			# Display word cloud
+					# 			plt.figure(figsize=(10, 7))
+					# 			plt.imshow(wordcloud_ip, interpolation='bilinear')
+					# 			plt.title(f'Word Cloud for {column}')
+					# 			plt.axis('off')
+					# 			plt.show()
+					# 			st.pyplot(plt, use_container_width=True)
+					# 		except Exception as e:
+					# 			st.write(f"Error generating word cloud: {e}")
+					# 	else:
+					# 		st.write(f"No words to plot for column '{column}'")
+					
 			elif pd.api.types.is_numeric_dtype(df[column]):
+				print("0a")
 				col3, col4 = st.columns(2)
 				with col3:
+					print("a")
 					st.write(f"  - Data Type: :green[{df[column].dtype}]")
+					print("b")
 					st.write(f"  - Mean: :green[{df[column].mean()}]")
 					st.write(f"  - Standard Deviation: :green[{df[column].std()}]")
 					st.write(f"  - Min Value: :green[{df[column].min()}]")
 					st.write(f"  - Max Value: :green[{df[column].max()}]")
 				with col4:
-					""" KDE plot """
+					# """ KDE plot """
 					plt.figure(figsize=(10, 6))  # Adjust figure size
 					sns.histplot(df[column], kde=True, color="green")
 					st.pyplot()
 					plt.close()
+	 				# KDE plot using Plotly
+					# fig = px.histogram(df, x=df[column], marginal="rug", nbins=30, histnorm='probability density', 
+                  	#  title="KDE Plot", color_discrete_sequence=['green'])
+					# # Create KDE plot using Plotly
+					# # Show the plot using Streamlit
+					# st.plotly_chart(fig, use_container_width=True)
+						# Create KDE plot using Plotly Express
+					# fig = px.histogram(df, x=df[column], marginal="rug", nbins=30, histnorm='probability density', 
+					# 				title="KDE Plot", color_discrete_sequence=['green'],opacity=0.5)
+
+					# # Add labels and annotations
+					# fig.update_layout(
+					# 	xaxis_title=column,
+					# 	yaxis_title="Density",
+					# 	showlegend=False,
+					# 	autosize=True,
+					# 	width=800,
+					# 	height=500,
+					# 	margin=dict(l=40, r=40, t=40, b=40),
+					# )
+
+					# # Customize the appearance of the KDE plot
+					# fig.update_traces(marker=dict(opacity=0.5))
+
+					# # Display the plot using Streamlit
+					# st.plotly_chart(fig, use_container_width=True)
 				with st.expander("More Info"):
 					# st.write("The chart above shows some numbers I picked for you.")
 					tab1, tab2, tab3 = st.tabs(["Insights", "Box plot", "QQ plot"])
@@ -405,20 +481,28 @@ try:
 						})
 
 						st.plotly_chart(fig)
-					   
-			st.write("---")
+		# st.text("hi")
+		# et = time.time()
+		# tt = et - s
+		# st.text(tt)
+			# st.write("hehe")
 	else:
 		st.write("DataFrame not found.")
 
-	st.write("``")
+		# st.write("``")
 
-except ZeroDivisionError:
+except ZeroDivisionError as ee:
+	print("ee", ee)
+	pass
+except AttributeError as eee:
+	print(eee)
 	pass
 except Exception as e:
 	# handle_error(e)
-    st.error(e)
-    st.subheader("⚠️Please uploade File⚠️")
-    pass
+	st.error(e)
+	st.subheader("⚠️Please uploade File⚠️")
+	print("f", e)
+	pass
 
 
 
